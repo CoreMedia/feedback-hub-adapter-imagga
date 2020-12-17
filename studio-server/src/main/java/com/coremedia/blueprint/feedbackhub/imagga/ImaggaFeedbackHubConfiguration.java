45e8b81e818575687f930c7c49b6fd992a6eedfd
Matthias Faust <matthias.faust@coremedia.com>
2020-12-17 09:42:35 +0100

    Config update

--- studio-server/src/main/java/com/coremedia/blueprint/feedbackhub/imagga/ImaggaFeedbackHubConfiguration.java
+++ studio-server/src/main/java/com/coremedia/blueprint/feedbackhub/imagga/ImaggaFeedbackHubConfiguration.java
@@ -1,18 +1,19 @@
 package com.coremedia.blueprint.feedbackhub.imagga;
 
 import com.coremedia.cache.Cache;
+import com.coremedia.cache.config.CacheConfiguration;
 import com.coremedia.feedbackhub.adapter.FeedbackHubAdapterFactory;
-import com.coremedia.springframework.xml.ResourceAwareXmlBeanDefinitionReader;
 import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Import;
 
 @Configuration(proxyBeanMethods = false)
-@ImportResource(value = {
-        "classpath:/com/coremedia/cache/cache-services.xml",
-}, reader = ResourceAwareXmlBeanDefinitionReader.class)@DefaultAnnotation(NonNull.class)
+@Import({
+        CacheConfiguration.class,
+})
+@DefaultAnnotation(NonNull.class)
 public class ImaggaFeedbackHubConfiguration {
   @Bean
   public FeedbackHubAdapterFactory imaggaFeedbackHubAdapterFactory(Cache cache) {
